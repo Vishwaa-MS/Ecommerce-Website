f95f4743bd721c0afe8255b1027a21c4a034bcf7
vishwaa <vishwaams.03@gmail.com>
2023-12-10 17:47:24 +0530

    modified:   controllers/admin.js 	modified:   controllers/auth.js 	modified:   routes/auth.js 	new file:   views/auth/new-password.ejs

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,7 +48,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
-        req.flash("error", "Invalid email or password.");
+        req.flash("error", "Invalid email.");
         return res.redirect("/login");
       }
       bcrypt
@@ -62,7 +62,7 @@ exports.postLogin = (req, res, next) => {
               res.redirect("/");
             });
           }
-          req.flash("error", "Invalid email or password.");
+          req.flash("error", "Invalid password.");
           res.redirect("/login");
         })
         .catch((err) => {
@@ -80,11 +80,8 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
-        req.flash(
-          "error",
-          "E-Mail exists already, please pick a different one."
-        );
-        return res.redirect("/signup");
+        req.flash("error", "E-Mail exists already, please login.");
+        return res.redirect("/login");
       }
       return bcrypt
         .hash(password, 12)
@@ -139,35 +136,84 @@ exports.getReset = (req, res, next) => {
 exports.postReset = (req, res, next) => {
   crypto.randomBytes(32, (err, buffer) => {
     if (err) {
-      console.log("NO Page");
       return res.redirect("/reset");
     }
     const token = buffer.toString("hex");
     User.findOne({ email: req.body.email })
       .then((user) => {
         if (!user) {
-          console.log("NO FOUND");
           req.flash("error", "No account with that email found");
           return res.redirect("/reset");
         }
         user.resetToken = token;
         user.resetTokenExpiration = Date.now() + 3600000;
-        return user.save();
-      })
-      .then((resukt) => {
-        res.redirect("/");
-        transporter.sendMail({
-          to: req.body.email,
-          from: "vishwaa2003@outlook.com",
-          subject: "Password Reset",
-          html: `
+        return user.save().then((result) => {
+          res.redirect("/");
+          transporter.sendMail({
+            to: req.body.email,
+            from: "vishwaa2003@outlook.com",
+            subject: "Password Reset",
+            html: `
           <p> You requested a password reset</p>
           <p>Click this <a href="http://localhost:3000/reset/${token}">link</a> to set a new password.</p>
           `,
+          });
         });
       })
       .catch((err) => {
         console.log(err);
       });
   });
+};
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then((user) => {
+      let message = req.flash("error");
+      if (message.length > 0) {
+        message = message[0];
+      } else {
+        message = null;
+      }
+      res.render("auth/new-password", {
+        path: "/reset-passwaord",
+        pageTitle: "New Password",
+        errorMessage: message,
+        userId: user._id.toString(),
+        passwordToken: token,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.postNewPassword = (req, res, next) => {
+  const newPassword = req.body.password;
+  const userId = req.body.userId;
+  const passwordToken = req.body.passwordToken;
+  let resetUser;
+
+  User.findOne({
+    resetToken: passwordToken,
+    resetTokenExpiration: { $gt: Date.now() },
+    _id: userId,
+  })
+    .then((user) => {
+      resetUser = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then((hashedPassword) => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      return resetUser.save();
+    })
+    .then((result) => {
+      res.redirect("/login");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 };
